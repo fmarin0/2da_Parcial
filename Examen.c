c4d15119244a67f439fd485cddaac4d1bb117548
fmarin0 <fatima_naomi_marin@hotmail.com>
2022-11-18 11:27:48 -0600

    De la segunda parcial

---
+++ Examen.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int pseudo(int list, int n)
+	int resultado;
+{
+        for (i=1,i>n-1,i++){
+                min = i;
+		return min;
+                for (j=i+1,j>n,i++)
+                        if (list[j] < list[min]) {
+                                min = j;
+
+			}
+	}
+	
+	if (indexMin!=i)
+
+        return
+	
+}
+
+void main (){
+
+	int n=0;
+	printf("Ingrese el valor del numero: ");
+	scanf("%d",&n);
+
+	int list=0;
+	pritf("He aqui el orden de los factores no altera el producto: ");
+	scanf("%d",&list);
+}
