dfc18b17beb9f4fc318f2f0639d95253376fc97c
fmarin0 <fatima_naomi_marin@hotmail.com>
2022-11-18 12:01:23 -0600

    Traduccion del Pseudocodigo

--- Examen.c
+++ Examen.c
@@ -1,14 +1,58 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int pseudo(int list, int n)
-	int resultado;
+int list[0];
+
+int ordenamiento(int list[], int n)
 {
-        for (i=1,i>n-1,i++){
-                min = i;
-		return min;
-                for (j=i+1,j>n,i++)
-                        if (list[j] < list[min]) {
-                                min = j;
+	int min=0;
+	int indexMin=0;
+	for(int i=0; i<n-1; i++)
+	{
+		min = i;
 
+		for(int j=i+1; j<n; j++){
+			if(list[j] < list[min]){
+				min = j;
 			}
+		}
+		if(indexMin !=i){
+			swap(&list[min], &list[i]);
+		}
 	}
+
+
+} 
+
+	int swap(int *x, int *y){
+		int temp = *x;
+		*x = *y;
+		*y = temp;
+	}
+
+int main(){
+	int n=0;
+	printf("Ingrese el tamaño del arreglo: ");
+	scanf("%d", &n);
+	int list[n];
+	for(int i=0; i<n; i++)
+	{
+		printf("Ingrese un valor: ");
+		scanf("%d", &list[i]);
+	
+	}
+	printf("Numeros desordenados: ");
+	for(int i=0; i<n; i++)
+	{
+		printf("%d, ", list[i]);
+	}
+	ordenamiento(list, n);
+	printf("Numeros ordenados:");
+	for(int i=0; i<n; i++)
+	{
+		printf("%d, ", list[i]);
+	}
+
+	return 0;
+
+}
